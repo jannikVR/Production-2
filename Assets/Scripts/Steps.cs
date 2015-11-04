3bcf5d4ab766975a4de5f3af27892a04dda107a5
Prawnkus <prawnkus@gmail.com>
2015-11-04 14:05:35 +0100

    Commit fra Version 1.37.5

---
+++ Assets/Scripts/Steps.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Steps : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetAxis ("Horizontal") != 0 || Input.GetAxis("Vertical") != 0){
+			this.GetComponent<SECTR_PointSource>().Play();
+		}
+	}
+}
